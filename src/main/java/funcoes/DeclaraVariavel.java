0369c26f38899700aaea839a996deb226168481d
ThiagoDevv <thiagofon2904@gmail.com>
2020-10-11 18:40:46 -0300

    Criação parcial das variáveis

--- src/main/java/funcoes/DeclaraVariavel.java
+++ src/main/java/funcoes/DeclaraVariavel.java
@@ -9,29 +9,60 @@ public class DeclaraVariavel implements DartHandler {
 			return;
 		}
 		if(line.startsWith("string")) {
-			line = line.replace("string", "");
-			line = line.replace("", "");
-		}
-		if (line.contains("var")) {
-			String[] nome = line.split(" ");
-			if (line.contains("double")) {
-				double value = 0;
-				doubles.put(nome[1], value);
-			} else if (line.contains("int")) {
-				int value = 0;
-				ints.put(nome[1], value);
-			} else {
-				String value = null;
-				strings.put(nome[1], value);
+			line = line.replace("string", "").replace(";", "").replace(" ","");
+			String [] partes = line.split("=");
+			String nome = partes[0];
+			String valor = "";
+			if(partes[1].startsWith("'") && partes[1].endsWith("'")) {
+				valor = partes[1].replace("'", "");
+				
+			
 			}
-		} else if (line.contains("num")) {
-			String[] nome = line.split(" ");
-			double value = 0;
-			doubles.put(nome[1], value);
-		} else if (line.contains("bool")) {
-			String[] nome = line.split(" ");
-			boolean value = false;
-			booleans.put(nome[1], value);
+			// TODO caso seja variável
+			//TODO caso seja input
+			//TODO colocar no map
+		}else if(line.startsWith("int")) {
+			line = line.replace("int", "").replace(";", "").replace(" ","");
+			String [] partes = line.split("=");
+			String nome = partes[0];
+			String valor = "";
+			// TODO caso seja variável
+			//TODO caso seja input
+			//TODO colocar no map
+		}else if(line.startsWith("double") || line.startsWith("num")) {
+			line = line.replace("double", "").replace("num", "").replace(";", "").replace(" ","");
+			String [] partes = line.split("=");
+			String nome = partes[0];
+			String valor = "";
+			// TODO caso seja variável
+			//TODO caso seja input
+			//TODO colocar no map
+		}else if(line.startsWith("boolean")) {
+			line = line.replace("boolean", "").replace(";", "").replace(" ","");
+			String [] partes = line.split("=");
+			String nome = partes[0];
+			String valor = "";
+			// TODO caso seja variável
+			//TODO caso seja input
+			//TODO colocar no map
+		}else if (line.contains("var")) {
+			line = line.replace("var", "").replace(";", "").replace(" ","");
+			String[] partes = line.split("=");
+			String nome = partes[0];
+			String valor = "";
+			// TODO caso seja variável
+			//TODO caso seja input
+			//TODO colocar no map
+//			if (line.contains("double")) {
+//				double value = 0;
+//				doubles.put(partes[1], value);
+//			} else if (line.contains("int")) {
+//				int value = 0;
+//				ints.put(partes[1], value);
+//			} else {
+//				String value = null;
+//				strings.put(partes[1], value);
+//			}
 		}
 	}
 }
